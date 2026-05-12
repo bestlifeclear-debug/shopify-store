bcce1b419bfcb7743f681cb357179c460852ff74
Ruslan <bestlifeclear@gmail.com>
2026-05-12 13:06:47 +0700

    remove shipping fee

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 /** Бесплатная доставка от суммы в ₽ */
-export const FREE_SHIPPING_THRESHOLD_RUB = 10_000
+export const FREE_SHIPPING_THRESHOLD_RUB = 0
 
 /** Фиксированная доставка в ₽, если порог не достигнут */
-export const FLAT_SHIPPING_RUB = 490
+export const FLAT_SHIPPING_RUB = 0
